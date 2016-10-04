f0dcafde06d970af5047173c8e75b32e74bdf30e
brendanMP <brendan.m.pete@gmail.com>
2016-10-04 15:33:21 -0400

    Additions to gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,54 +1,107 @@
-// Load Node Modules/Plugins
-import gulp from 'gulp';
-import gulpLoadPlugins from 'gulp-load-plugins';
-import browserSync from 'browser-sync';
-import del from 'del';
-var gutil = require('gulp-util');
+var gulp = require('gulp'),
+    environments = require('gulp-environments'),
+    del = require('del'),
+    sass = require('gulp-sass'),
+    prefixer = require('gulp-autoprefixer'),
+    uncss = require('gulp-uncss'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync'),
+    changed = require('gulp-changed'),
+    minimizer = require('gulp-imagemin'),
+    size = require('gulp-size'),
+    babel = require('gulp-babel'),
+    sourcemaps = require("gulp-sourcemaps"),
+    concat = require('gulp-concat-js');
 
+var production = environments.production;
 
-const reload = browserSync.reload;
-const $ = gulpLoadPlugins();
-
-// Remove existing docs and dist build
-gulp.task('clean', del.bind(null, ['docs/dist', 'dist']));
-
-
-// Build LibSass files
-gulp.task('styles', function() {
-    gulp.src('./scss/bootstrap.scss')
-        .pipe($.plumber())
-        .pipe($.sourcemaps.init())
-        .pipe($.sass().on('error', $.sass.logError))
-        .pipe($.sourcemaps.write('.'))
-        .pipe($.autoprefixer({browsers: ['last 1 version']}))
-        .pipe($.rename({ suffix: '.min' }))
+gulp.task('styles', function () {
+    gulp.src('./src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: [
+                './node_modules/bootstrap/scss'
+            ],
+            style: 'uncompressed',
+            quiet: true
+        }).on('error', sass.logError))
+        .pipe(production(uncss({
+            html: ['dist/**/*.html']
+        })))
+        .pipe(prefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(production(cleanCSS()))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist/css'))
-        .pipe($.minifyCss())
-        .pipe(gulp.dest('dist/css'));
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src([
+            './node_modules/bootstrap/js/dist/util.js',
+            './node_modules/bootstrap/js/dist/alert.js',
+            './node_modules/bootstrap/js/dist/button.js',
+            './node_modules/bootstrap/js/dist/carousel.js',
+            './node_modules/bootstrap/js/dist/collapse.js',
+            './node_modules/bootstrap/js/dist/dropdown.js',
+            './node_modules/bootstrap/js/dist/modal.js',
+            './node_modules/bootstrap/js/dist/scrollspy.js',
+            './node_modules/bootstrap/js/dist/tab.js',
+            './node_modules/bootstrap/js/dist/tooltip.js',
+            './node_modules/bootstrap/js/dist/popover.js',
+            './src/js/**/*.js'
+        ])
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(concat({
+            "target": "app.js",
+            "entry": "./src/js/app.js"
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('html', function () {
+    gulp.src('./src/**/**/*.html')
+        .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('images', function () {
+    return gulp.src('src/img/**')
+        .pipe(changed('dist/img'))
+        .pipe(minimizer({progressive: true, interlaced: true}))
+        .pipe(gulp.dest('dist/img'))
+        .pipe(size({title: 'images'}));
 });
 
 
 
-// Build JavaScript files
-gulp.task('scripts', function() {
-    return gulp.src(['js/src/util.js', 'js/src/alert.js', 'js/src/button.js', 'js/src/carousel.js', 'js/src/collapse.js', 'js/src/dropdown.js', 'js/src/modal.js', 'js/src/scrollspy.js', 'js/src/tab.js', 'js/src/tooltip.js', 'js/src/popover.js'])
-        .pipe($.babel())
-        .pipe($.concat('./bootstrap.js'))
-        .pipe(gulp.dest('dist/js'));
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            baseDir: "./dist/",
+            injectChanges: true,
+            browser: "Google Chrome"
+        }
+    });
 });
 
-
-
-
-
-// Watch tasks
-gulp.task('watch', function() {
-    gulp.watch('scss/*.scss', ['styles']);
-    gulp.watch('js/src/*.js', ['scripts']);
+gulp.task('clean', function () {
+    return del([
+        'dist/**/*'
+    ]);
 });
 
-gulp.task('dist', ['styles', 'scripts']);
+gulp.task('watch', function () {
+    gulp.watch("src/js/**/*.js", [browserSync.reload]);
+    gulp.watch('src/**/*.html', ['html', browserSync.reload]);
+    gulp.watch('src/scss/**/*.scss', ['styles', browserSync.reload]);
+    gulp.watch('src/images/**/*', ['images', browserSync.reload]);
+});
 
-gulp.task('default', ['clean'], () => {
-    gulp.start('dist');
+gulp.task('default', function () {
+    gulp.start('clean', 'styles', 'scripts', 'images', 'html', 'browser-sync', 'watch');
 });
