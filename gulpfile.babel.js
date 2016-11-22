8f959a40bfe8cfe7c8819869fd4028ac64006b27
Brendan Pete <brendan.m.pete@gmail.com>
2016-11-22 12:56:43 -0500

    Gh-pages prep

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -39,7 +39,8 @@ gulp.task('styles', function () {
 gulp.task('scripts', function () {
     return gulp.src([
             './node_modules/bootstrap/js/dist/util.js',
-            './node_modules/bootstrap/js/dist/alert.js',
+            './node_modules/bootstrap/js/dist/util.js',
+            './node_modules/bootstrap/js/dist/alert.js' ,
             './node_modules/bootstrap/js/dist/button.js',
             './node_modules/bootstrap/js/dist/carousel.js',
             './node_modules/bootstrap/js/dist/collapse.js',
